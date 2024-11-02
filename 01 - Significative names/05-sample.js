23eeab40efbfc0865064a9473e0ea3753f5c3047
Shizaricchi <166448916+Shizaricchi@users.noreply.github.com>
2024-11-02 17:22:10 -0300

    Update 05-sample.js

--- 01 - Significative names/05-sample.js
+++ 01 - Significative names/05-sample.js
@@ -1,11 +1,11 @@
-let a = "João";
-let b = "Ana";
-let c = "Carlos";
+let nome01 = "João";
+let nome02 = "Ana";
+let nome03 = "Carlos";
 
-function p() {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+function printar() {
+    console.log(nome01);
+    console.log(nome02);
+    console.log(nome03);
 }
 
-p();
+printar();
